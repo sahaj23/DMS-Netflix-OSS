7f02353ba6c6d7f96603bb120e27c8d7d9c8ef18
sahaj23 <khandelwalsahaj36@gmail.com>
2020-09-30 08:11:28 +0530

    AWS S3 integration done

--- dms-poc/dms-create/src/main/java/com/techprimers/cloud/controller/DmsCreateController.java
+++ dms-poc/dms-create/src/main/java/com/techprimers/cloud/controller/DmsCreateController.java
@@ -1,7 +1,8 @@
 package com.techprimers.cloud.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.techprimers.cloud.model.DmsCreateModel;
 import com.techprimers.cloud.repository.DmsCreateRepository;
 
@@ -35,7 +45,7 @@ public class DmsCreateController {
         return "uploadform";
     }
 
-    @HystrixCommand(fallbackMethod = "fallbackUpload")
+//    @HystrixCommand(fallbackMethod = "fallbackUpload")
     @PostMapping("/")
     public ResponseEntity<String> uploadMultipartFile(@RequestParam("files") MultipartFile[] files, Model model) {
         List<String> fileNames = new ArrayList<String>();
@@ -45,32 +55,40 @@ public class DmsCreateController {
             List<DmsCreateModel> storedFile = new ArrayList<DmsCreateModel>();
             for (MultipartFile file : files) {
             
-            	String url = "http://dms-search/rest/dms-search/searchDocument/fileSearch/"+file.getOriginalFilename()+"/";
-            	System.out.println("Original "+file.getOriginalFilename());
-            	DmsCreateModel fileModel = restTemplate.getForObject(url, DmsCreateModel.class);
-            	System.out.println("reached  here");
-                if (fileModel != null) {
-                    try {
-						fileModel.setPic(file.getBytes());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-                    fileModel.setCreatedAt(new Timestamp(new java.util.Date().getTime()));
-                } else {
-                    try {
-						fileModel = new DmsCreateModel(file.getOriginalFilename(), file.getContentType(), file.getBytes(),new Timestamp(new java.util.Date().getTime()));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-                }
-
-                fileNames.add(file.getOriginalFilename());
-                storedFile.add(fileModel);
+//            	String url = "http://dms-search/rest/dms-search/searchDocument/fileSearch/"+file.getOriginalFilename()+"/";
+//            	System.out.println("Original "+file.getOriginalFilename());
+//            	DmsCreateModel fileModel = restTemplate.getForObject(url, DmsCreateModel.class);
+//            	System.out.println("reached  here");
+//                if (fileModel != null) {
+//                    try {
+//						fileModel.setPic(file.getBytes());
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//                    fileModel.setCreatedAt(new Timestamp(new java.util.Date().getTime()));
+//                } else {
+//                    try {
+//						fileModel = new DmsCreateModel(file.getOriginalFilename(), file.getContentType(), file.getBytes(),new Timestamp(new java.util.Date().getTime()));
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//                }
+//
+//                fileNames.add(file.getOriginalFilename());
+//                storedFile.add(fileModel);
+            	
+            	//File fileToUpload = (File) file;
+    			try {
+					uploadFileToAmazonS3(convert(file));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
             }
 
-            fileRepository.saveAll(storedFile);
+//            fileRepository.saveAll(storedFile);
             model.addAttribute("message", "Files uploaded successfully!");
             model.addAttribute("files", fileNames);
 //        } catch (Exception e) {
@@ -82,6 +100,39 @@ public class DmsCreateController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Document uploaded successfully!!");
     }
+    
+    public static File convert(MultipartFile file) throws IOException {
+    	File convFile = new File(file.getOriginalFilename());
+    	convFile.createNewFile();
+    	FileOutputStream fos = new FileOutputStream(convFile);
+    	fos.write(file.getBytes());
+    	fos.close();
+    	return convFile;
+    }
+    
+    private void uploadFileToAmazonS3(File file) {
+		String bucketName = "test-s3withlambdaa";
+		String accesskey = "AKIA4TML7TVYWLU3TBQ2";
+		String secretkey = "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";
+
+		try {
+			AWSCredentials credentials = new BasicAWSCredentials(accesskey, secretkey);
+			AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+					.withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.AP_SOUTH_1)
+					.build();
+
+			PutObjectRequest request = new PutObjectRequest(bucketName, file.getName(), file);
+			ObjectMetadata metadata = new ObjectMetadata();
+			metadata.setContentType("plain/text");
+			request.setMetadata(metadata);
+			s3Client.putObject(request);
+
+		} catch (AmazonServiceException e) {
+			e.printStackTrace();
+		} catch (SdkClientException e) {
+			e.printStackTrace();
+		}
+	}
     
     public ResponseEntity<String> fallbackUpload(@RequestParam("files") MultipartFile[] files, Model model){
     	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Document could not be uploaded! One or more services might be down! Try again later");
--- dms-poc/eureka-service/src/main/java/com/techprimers/cloud/EurekaServiceApplication.java
+++ dms-poc/eureka-service/src/main/java/com/techprimers/cloud/EurekaServiceApplication.java
@@ -3,10 +3,11 @@ package com.techprimers.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class ,  ContextRegionProviderAutoConfiguration.class})
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
